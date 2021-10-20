735b992db9d3f677453ef9287471a0ec2e12d66a
Emilio Lopez Berenguer <emilio@emiliollbb.net>
2021-10-20 12:30:04 +0200

    Engine

--- JEngine.cpp
+++ JEngine.cpp
@@ -71,187 +71,185 @@ void JEngine::init() {
     }
   
   
-  //Create renderer for window
-  sdl_renderer = SDL_CreateRenderer( sdl_window, -1, SDL_RENDERER_ACCELERATED );
-  if( sdl_renderer == NULL )
-  {
-    printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
-    exit(-1);
-  }
+    //Create renderer for window
+    sdl_renderer = SDL_CreateRenderer( sdl_window, -1, SDL_RENDERER_ACCELERATED );
+    if( sdl_renderer == NULL )
+    {
+      printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+      exit(-1);
+    }
   
-  //Initialize SDL_ttf 
-  if(TTF_Init()<0) 
-  {
-    printf( "SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError() ); 
-    exit(-1);
-  }
+    //Initialize SDL_ttf 
+    if(TTF_Init()<0) 
+    {
+      printf( "SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError() ); 
+      exit(-1);
+    }
   
-  //Initialize PNG loading
-  if( !( IMG_Init( IMG_INIT_PNG ) & IMG_INIT_PNG ) )
-  {
-    printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
-    exit(-1);
-  }
+    //Initialize PNG loading
+    if( !( IMG_Init( IMG_INIT_PNG ) & IMG_INIT_PNG ) )
+    {
+      printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+      exit(-1);
+    }
   
-  //Initialize SDL_mixer 
-  if(Mix_OpenAudio( 22050, MIX_DEFAULT_FORMAT, 2, 512 )<0) 
-  { 
-    printf( "SDL_mixer could not initialize!\n");
-    exit(-1);
-  }
-  
+    //Initialize SDL_mixer 
+    if(Mix_OpenAudio( 22050, MIX_DEFAULT_FORMAT, 2, 512 )<0) 
+    { 
+      printf( "SDL_mixer could not initialize!\n");
+      exit(-1);
+    }
 }
 
 void JEngine::close() {
     cout << "Close" << endl;
     
-   // Close media
-   close_media(); 
+    // Close media
+    close_media(); 
   
-  //Destroy renderer  
-  if(sdl_renderer!=NULL)
-  {
-    SDL_DestroyRenderer( sdl_renderer );
-    sdl_renderer=NULL;
-  }
+    //Destroy renderer  
+    if(sdl_renderer!=NULL)
+    {
+      SDL_DestroyRenderer( sdl_renderer );
+      sdl_renderer=NULL;
+    }
   
-  if(sdl_window != NULL)
-  {
-    // Destroy window
-    SDL_DestroyWindow( sdl_window );
-    sdl_window=NULL;
-  }
+    if(sdl_window != NULL)
+    {
+      // Destroy window
+      SDL_DestroyWindow( sdl_window );
+      sdl_window=NULL;
+    }
   
-  // Close gamepads
-  for(int i=0; i<SDL_NumJoysticks(); i++)
-  {
-    SDL_JoystickClose(sdl_gamepads[i]);
-    sdl_gamepads[i]=NULL;
-  }
+    // Close gamepads
+    for(int i=0; i<SDL_NumJoysticks(); i++)
+    {
+      SDL_JoystickClose(sdl_gamepads[i]);
+      sdl_gamepads[i]=NULL;
+    }
   
-  // Exit SDL
-  Mix_CloseAudio();
-  TTF_Quit();
-  IMG_Quit();
-  SDL_Quit();
+    // Exit SDL
+    Mix_CloseAudio();
+    TTF_Quit();
+    IMG_Quit();
+    SDL_Quit();
 }
 
 void JEngine::process_input(SDL_Event *e)
 {
-  //User requests quit
-  if(e->type == SDL_QUIT 
-    // User press ESC or q
-    || (e->type == SDL_KEYDOWN && (e->key.keysym.sym=='q' || e->key.keysym.sym == 27))
-  )
-  {
-    quit = 1;
-  }
+    //User requests quit
+    if(e->type == SDL_QUIT 
+      // User press ESC or q
+      || (e->type == SDL_KEYDOWN && (e->key.keysym.sym=='q' || e->key.keysym.sym == 27))
+    )
+    {
+      quit = 1;
+    }
 }
 
 void JEngine::sync_render()
 {
-  unsigned int start, end, render_time; 
+    unsigned int start, end, render_time; 
   
-  start = SDL_GetTicks();
+    start = SDL_GetTicks();
   
-  // Count frames
-  frames++;
-  // Update game data
-  update_game();
+    // Count frames
+    frames++;
+    // Update game data
+    update_game();
   
-  // Render screen
-  render();  
+    // Render screen
+    render();  
   
   
-  end = SDL_GetTicks();
-  render_time = end - start;
+    end = SDL_GetTicks();
+    render_time = end - start;
   
-  // 60 fps -> 16ms
-  // 30 fps -> 32ms
-  // 50 fps -> 20ms
-  // 100 fps -> 10ms
-  
-  if(render_time < 20)
-  {
-    //remaining = 1;
-    SDL_Delay(20 - render_time);
-  }
-  else
-  {
-    printf("Render time: %ud !!!!\n", render_time);
-  }  
+    // 60 fps -> 16ms
+    // 30 fps -> 32ms
+    // 50 fps -> 20ms
+    // 100 fps -> 10ms
+    if(render_time < 20)
+    {
+      //remaining = 1;
+      SDL_Delay(20 - render_time);
+    }
+    else
+    {
+      printf("Render time: %ud !!!!\n", render_time);
+    }  
 }
 
 void JEngine::run()
 {
-  //Event handler
-  SDL_Event e;
+    //Event handler
+    SDL_Event e;
   
-  // Init quit flag
-  quit=0;
+    // Init quit flag
+    quit=0;
   
-  // Init SDL
-  init();
+    // Init SDL
+    init();
   
-  // Load Media
-  cout << "Load media..." <<endl;
-  load_media();
+    // Load Media
+    cout << "Load media..." <<endl;
+    load_media();
       
-  // Main game loop
-  while(!quit)
-  {
-    //Handle events on queue
-    while( SDL_PollEvent( &e ) != 0 )
+    // Main game loop
+    while(!quit)
     {
-      process_input(&e);
-    }
+      //Handle events on queue
+      while( SDL_PollEvent( &e ) != 0 )
+      {
+        process_input(&e);
+      }
     
-    // Update & Render
-    sync_render();
-  }
+      // Update & Render
+      sync_render();
+    }
   
-  // Close media
-  close_media();
-  // Close SDL
-  close();
+    // Close media
+    close_media();
+    // Close SDL
+    close();
 }
 
 void JEngine::render() {
     //Clear screen
-  SDL_SetRenderDrawColor( sdl_renderer, 0x00, 0x00, 0x00, 0xFF );
-  SDL_RenderClear( sdl_renderer );
+    SDL_SetRenderDrawColor( sdl_renderer, 0x00, 0x00, 0x00, 0xFF );
+    SDL_RenderClear( sdl_renderer );
   
-  // Actual rendering
-  render_game();
+    // Actual rendering
+    render_game();
   
-  //Update screen
-  SDL_RenderPresent(sdl_renderer);
+    //Update screen
+    SDL_RenderPresent(sdl_renderer);
 }
 
 void JEngine::load_texture(struct sized_texture *texture, string path)
 {
-  // Aux surface
-  SDL_Surface* loadedSurface;
+    // Aux surface
+    SDL_Surface* loadedSurface;
   
-  //Load image at specified path
-  loadedSurface = IMG_Load(path.c_str());
-  if(loadedSurface == NULL )
-  {
-    printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
-    exit(-1);
-  }
-  //Get image dimensions 
-  texture->width = loadedSurface->w; 
-  texture->height = loadedSurface->h;
-  //Create texture from surface pixels
-  texture->texture = SDL_CreateTextureFromSurface(sdl_renderer, loadedSurface);
-  if( texture->texture == NULL )
-  {
-    printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
-  }
+    //Load image at specified path
+    loadedSurface = IMG_Load(path.c_str());
+    if(loadedSurface == NULL )
+    {
+      printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+      exit(-1);
+    }
+    //Get image dimensions 
+    texture->width = loadedSurface->w; 
+    texture->height = loadedSurface->h;
+    //Create texture from surface pixels
+    texture->texture = SDL_CreateTextureFromSurface(sdl_renderer, loadedSurface);
+    if( texture->texture == NULL )
+    {
+      printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+    }
   
-  //Get rid of old loaded surface
-  SDL_FreeSurface(loadedSurface);
+    //Get rid of old loaded surface
+    SDL_FreeSurface(loadedSurface);
   
 }
 
